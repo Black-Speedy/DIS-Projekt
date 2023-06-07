e334029bd40f6d111e4f84bc87898afc6977ec60
rxv292 <100592309+rxv292@users.noreply.github.com>
2023-06-07 16:08:16 +0200

    more things

--- pokemon-showdown-scraper-master/pokemon_egg_scraper.py
+++ pokemon-showdown-scraper-master/pokemon_egg_scraper.py
@@ -2,36 +2,28 @@ import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://dex.pokemonshowdown.com/pokemon/")
 search_input = browser.find_element_by_class_name("searchbox")
 
-file = open("./pokemon-data/pokemon-evolution-data.csv", 'w')
-file.write("Pokemon, Pre-evolution, Method \n")
+file = open("./pokemon-data/pokemon-egg-data.csv", 'w')
+file.write("Pokemon, Egg-Group \n")
 
 with open('./names.json') as f:
   pokemon_names = json.loads(f.read())
 
 for name in pokemon_names[0:1068]:
-
   search_input.clear()
   search_input.send_keys(name)
   search_input.send_keys(Keys.RETURN)
 
   pokemon = name
 
-
-  evo = browser.find_elements_by_tag_name("small")
-  for elm in evo:
-    if "Evolves" in elm.text:
-      pre_met = (elm.text).removeprefix("Evolves from ")
-      prevolution = (pre_met.split(" ", 1)[0]).replace("é", "e").replace("’", "'")
-      method = (pre_met.split(" (", 1)[1]).removesuffix(")")
-
-      file.write(pokemon + "," + prevolution + "," + method + "\n")
-      break
+  egg_groups = browser.find_elements_by_class_name('colentry')
+  eggs = (egg_groups[0].text).removeprefix("Egg groups:\n")
+  for egg in (eggs.split(", ", 1)):
+    file.write(pokemon + "," + egg + "\n")
 
 browser.close()
 file.close()
--- pokemon-showdown-scraper-master/pokemon_move_scraper.py
+++ pokemon-showdown-scraper-master/pokemon_move_scraper.py
@@ -2,36 +2,33 @@ import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://dex.pokemonshowdown.com/pokemon/")
 search_input = browser.find_element_by_class_name("searchbox")
 
-file = open("./pokemon-data/pokemon-evolution-data.csv", 'w')
-file.write("Pokemon, Pre-evolution, Method \n")
+file = open("./pokemon-data/pokemon-move-data.csv", 'w')
+file.write("Pokemon, Move, Source \n")
 
 with open('./names.json') as f:
   pokemon_names = json.loads(f.read())
 
 for name in pokemon_names[0:1068]:
-
   search_input.clear()
   search_input.send_keys(name)
   search_input.send_keys(Keys.RETURN)
 
   pokemon = name
 
+  
+  moves = browser.find_elements_by_class_name('resultheader')
+  print(moves[0].text)
+  print(moves[1].text)
 
-  evo = browser.find_elements_by_tag_name("small")
-  for elm in evo:
-    if "Evolves" in elm.text:
-      pre_met = (elm.text).removeprefix("Evolves from ")
-      prevolution = (pre_met.split(" ", 1)[0]).replace("é", "e").replace("’", "'")
-      method = (pre_met.split(" (", 1)[1]).removesuffix(")")
 
-      file.write(pokemon + "," + prevolution + "," + method + "\n")
-      break
+  
+
+  #file.write(pokemon + "," + move + "," + source + "\n")
 
 browser.close()
 file.close()
