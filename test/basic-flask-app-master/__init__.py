62ccd005b3a30fd24a526e2882478de04f97c1be
rxv292 <100592309+rxv292@users.noreply.github.com>
2023-06-10 03:32:03 +0200

    little thing

--- test/basic-flask-app-master/__init__.py
+++ test/basic-flask-app-master/__init__.py
@@ -18,7 +18,7 @@ app.config['SECRET_KEY'] = 'fc089b9218301ad987914c53481bff04'
 
 # set your own database
 #db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'UIS'"
-db = "dbname='postgres' user='postgres' host='localhost' password = 'miramax1'"
+db = "dbname='postgres' user='postgres' host='localhost' password = ''"
 conn = psycopg2.connect(db)
 
 bcrypt = Bcrypt(app)
